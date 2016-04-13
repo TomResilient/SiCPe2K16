3bebb3181c30807430d4b48aadc74bde36e8e4f3
TomResilient <salvadorthomas.pro@gmail.com>
2016-04-13 10:27:57 +0200

    FOS Commit

---
+++ src/Platform/UserBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php 
+
+// src/Platform/UserBundle/User.php 
+
+namespace Platform\UserBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+* @ORM\Entity
+* @ORM\Table(name="si_user")
+*/
+
+class User extends BaseUser{
+
+/**
+* @ORM\id
+* @ORM\Column(type="integer")
+* @ORM\GeneratedValue(strategy="AUTO")
+*/
+
+	protected $id;
+
+}
+
+?>
